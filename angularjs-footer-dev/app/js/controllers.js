087dab73b6e794d1316826af9487949ecc855492
David Jimenez-Morales <biodavidjm@gmail.com>
2014-08-28 17:06:19 -0500

    Basic dynamically generated menu and description
    Playing with AngularJS: it generates a menu from an array defined in
    the controller.

---
+++ angularjs-footer-dev/app/js/controllers.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* Controllers */
+
+var dictycatApp = angular.module('dictycatApp', []);
+
+dictycatApp.controller('DictyMenuListCtrl', function($scope) {
+  $scope.title = "Angular dictyBase: the Footer";
+  $scope.sections = [
+    {'name': 'Genomes',
+     'snippet': 'Access to the dicty genomes available at the dictyweb'},
+    {'name': 'Tools',
+     'snippet': 'Access the dicty data through our set of tools'},
+    {'name': 'dictyAccess',
+     'snippet': 'The next generation tool for the anlysis of dicty genomes'},
+    {'name': 'Stock Center',
+ 	 'snippet': 'The central central repository for Dictyostelium discoideum strains, isolates of other cellular slime mold species, plasmids, and commonly used bacteria'},
+	{'name': 'Research', 
+	 'snippet': 'All about Dictyostelium: protocols, techniques, anatomy ontology, etc'},
+	 {'name': 'Community',
+	 'snippet': 'Access the information about the highly active dicty community'}
+  ];
+});
