8f3aeaf1c1c9ab199f83dad8c291e4c84a9e7b91
David Jimenez-Morales <biodavidjm@gmail.com>
2014-09-30 17:01:39 -0500

    dicty-footer directive: stylesheet implementations
    * IIFE
    * simple setter syntax for modules, controller and directive

--- dicty-footer/dynamic-footer/footer_minimal/scripts/app.js
+++ dicty-footer/dynamic-footer/footer_minimal/scripts/app.js
@@ -1,8 +1,6 @@
-// 'use strict';
-
 /**
  * @ngdoc overview
- * @name angularjsFooterDevApp
+ * @name dictyFooterApp
  * @description
  * # angularjsFooterDevApp  
  *
@@ -12,119 +10,117 @@
 
 (function(){
 
-    var dictyApp = angular.module('dictyFooterApp', []);
+    'use strict';
 
-    dictyApp.controller('dictyFooterCtrl', function(){
-        this.whatevers = thelinks;
-    });
+    angular.module('dictyFooterApp', []);
 
-    dictyApp.controller('dictyFooterCtrl', ['$scope', function ($scope) {
+    angular.module('dictyFooterApp').controller('dictyFooterCtrl', dictyFooterCtrl);
 
-    	$scope.sections = {};
+    function dictyFooterCtrl($scope) {
+        $scope.title = 'Dynamic Footer (this is dynamic itself)';
+        $scope.sections = {};
 
-        $scope.sections.menus = this;
-
-    	$scope.sections.menus = [
-        {
-            "main": "Genomes", 
-            "submain": {
-                "D. discoideum": "#",
-                "D. fasciculatum": "#",
-                "D. purpureum": "#",
-                "P. pallidum": "#"
+        $scope.sections.menus = [
+            {
+                "main": "Genomes", 
+                "submain": {
+                    "D. discoideum": "#",
+                    "D. fasciculatum": "#",
+                    "D. purpureum": "#",
+                    "P. pallidum": "#"
+                }
+            }, 
+            {
+                "main": "Tools", 
+                "submain": {
+                    "DictyMine": "#",
+                    "Signaling Pathways": "#",
+                    "Genome Browser": "#",
+                    "BLAST": "#",
+                    "ID Converter": "#",
+                    "dictyMart": "#",
+                    "Textpresso": "#",
+                    "Biochemical Pathways": "#",
+                    "Third Party Tools": "#"
+                }
+            }, 
+            {
+                "main": "DictyAccess", 
+                "submain": {
+                    "Genome Eye View": "#",
+                    "Genomic statistics": "#",
+                    "Annotations": "#",
+                    "Ontology": "#",
+                    "Phylogeny": "#"
+                }
+            }, 
+            {
+                "main": "Stock Center", 
+                "submain": {
+                    "Home": "#",
+                    "About SC": "#",
+                    "Search SC": "#",
+                    "Order": "#",
+                    "Deposit": "#",
+                    "Strain Catalog": "#",
+                    "Plasmid Catalog": "#",
+                    "Additional Materials": "#",
+                    "SC FAQ": "#",
+                    "Other SC": "#"
+                }
+            }, 
+            {
+                "main": "Research", 
+                "submain": {
+                    "Learn about Dicty": "#",
+                    "Teaching Protocols": "#",
+                    "Techniques": "#",
+                    "Anatomy Ontology": "#",
+                    "Mutant Phenotypes": "#",
+                    "HTP Phenotyping at Princeton": "#",
+                    "Codon Bias Table": "#",
+                    "Nomenclature Guidelines": "#",
+                    "Axenic Strains History": "#",
+                    "Franke Dicty Reference Library": "#",
+                    "Genome Resources": "#"
+                }
+            },
+            {
+                "main": "Community", 
+                "submain": {
+                    "Submit an Abstract": "#",
+                    "Dicty News": "#",
+                    "Listserv Archive": "#",
+                    "Colleagues profiles": "#",
+                    "Dicty Anual Conference": "#",
+                    "Job Opportunities": "#",
+                    "History": "#",
+                    "DictyLabs web": "#",
+                    "Pictures/Videos": "#",
+                    "dictyArt": "#",
+                    "Useful Links": "#",
+                    "Virtual Library": "#"
+                }
             }
-        }, 
-        {
-            "main": "Tools", 
-            "submain": {
-                "DictyMine": "#",
-                "Signaling Pathways": "#",
-                "Genome Browser": "#",
-                "BLAST": "#",
-                "ID Converter": "#",
-                "dictyMart": "#",
-                "Textpresso": "#",
-                "Biochemical Pathways": "#",
-                "Third Party Tools": "#"
-            }
-        }, 
-        {
-            "main": "DictyAccess", 
-            "submain": {
-                "Genome Eye View": "#",
-                "Genomic statistics": "#",
-                "Annotations": "#",
-                "Ontology": "#",
-                "Phylogeny": "#"
-            }
-        }, 
-        {
-            "main": "Stock Center", 
-            "submain": {
-                "Home": "#",
-                "About SC": "#",
-                "Search SC": "#",
-                "Order": "#",
-                "Deposit": "#",
-                "Strain Catalog": "#",
-                "Plasmid Catalog": "#",
-                "Additional Materials": "#",
-                "SC FAQ": "#",
-                "Other SC": "#"
-            }
-        }, 
-        {
-            "main": "Research", 
-            "submain": {
-                "Learn about Dicty": "#",
-                "Teaching Protocols": "#",
-                "Techniques": "#",
-                "Anatomy Ontology": "#",
-                "Mutant Phenotypes": "#",
-                "HTP Phenotyping at Princeton": "#",
-                "Codon Bias Table": "#",
-                "Nomenclature Guidelines": "#",
-                "Axenic Strains History": "#",
-                "Franke Dicty Reference Library": "#",
-                "Genome Resources": "#"
-            }
-        },
-        {
-            "main": "Community", 
-            "submain": {
-                "Submit an Abstract": "#",
-                "Dicty News": "#",
-                "Listserv Archive": "#",
-                "Colleagues profiles": "#",
-                "Dicty Anual Conference": "#",
-                "Job Opportunities": "#",
-                "History": "#",
-                "DictyLabs web": "#",
-                "Pictures/Videos": "#",
-                "dictyArt": "#",
-                "Useful Links": "#",
-                "Virtual Library": "#"
-            }
-        }
-    	];
+        ];
+    };
 
-    	$scope.title = 'Dynamic Footer (this is dynamic itself)';
-
-    }]);
-
-    dictyApp.directive('firstDirective', function(){
-        return{
-            restrict: 'E',
-            template: "<h3><strong>Yep: this is a directive</strong></h3>"
+    angular
+        .module('dictyFooterApp')
+        .directive('firstDirective', function(){
+            return{
+                restrict: 'E',
+                template: "<h3><strong>Yep: this is a directive</strong></h3>"
         };
     });
 
-    dictyApp.directive('dictyFooter', function(){
-        return{
-            restrict:'EA',
-            templateUrl:"templates/dicty-footer.html"
+    angular
+        .module('dictyFooterApp')
+        .directive('dictyFooter', function(){
+            return{
+                restrict:'EA',
+                templateUrl:"templates/dicty-footer.html"
         };
     });
 
 })();
-
