48e6cff976df5cc412faf778827a4b3ff3d77a0a
David Jimenez-Morales <biodavidjm@gmail.com>
2014-10-07 12:04:23 -0500

    Separating the data from the code
    - Using the $http service, which facilitates communication with the
    remote http servers.
    - It imports the data from the `links.json` file

--- dicty-footer/dynamic-footer/footer_minimal/scripts/controllers/dicty.footer.controller.js
+++ dicty-footer/dynamic-footer/footer_minimal/scripts/controllers/dicty.footer.controller.js
@@ -1,7 +1,8 @@
 /**
  dicty.footer.controller.js
  ----------------
- dictyFooterCtrl
+  dictyFooterCtrl: the controller of the footer
+  
  */
 
 (function () {
@@ -11,92 +12,12 @@
         .module('dictyFooterApp')
         .controller('dictyFooterCtrl', dictyFooterCtrl);
 
-    function dictyFooterCtrl($scope) {
+    function dictyFooterCtrl($scope, $http) {
         $scope.title = 'Dynamic Footer (this is dynamic itself)';
         $scope.sections = {};
-        $scope.sections.menus = [
-            {
-                "main": "Genomes", 
-                "submain": {
-                    "D. discoideum": "#",
-                    "D. fasciculatum": "#",
-                    "D. purpureum": "#",
-                    "P. pallidum": "#"
-                }
-            }, 
-            {
-                "main": "Tools", 
-                "submain": {
-                    "DictyMine": "#",
-                    "Signaling Pathways": "#",
-                    "Genome Browser": "#",
-                    "BLAST": "#",
-                    "ID Converter": "#",
-                    "dictyMart": "#",
-                    "Textpresso": "#",
-                    "Biochemical Pathways": "#",
-                    "Third Party Tools": "#"
-                }
-            }, 
-            {
-                "main": "DictyAccess", 
-                "submain": {
-                    "Genome Eye View": "#",
-                    "Genomic statistics": "#",
-                    "Annotations": "#",
-                    "Ontology": "#",
-                    "Phylogeny": "#"
-                }
-            }, 
-            {
-                "main": "Stock Center", 
-                "submain": {
-                    "Home": "#",
-                    "About SC": "#",
-                    "Search SC": "#",
-                    "Order": "#",
-                    "Deposit": "#",
-                    "Strain Catalog": "#",
-                    "Plasmid Catalog": "#",
-                    "Additional Materials": "#",
-                    "SC FAQ": "#",
-                    "Other SC": "#"
-                }
-            }, 
-            {
-                "main": "Research", 
-                "submain": {
-                    "Learn about Dicty": "#",
-                    "Teaching Protocols": "#",
-                    "Techniques": "#",
-                    "Anatomy Ontology": "#",
-                    "Mutant Phenotypes": "#",
-                    "HTP Phenotyping at Princeton": "#",
-                    "Codon Bias Table": "#",
-                    "Nomenclature Guidelines": "#",
-                    "Axenic Strains History": "#",
-                    "Franke Dicty Reference Library": "#",
-                    "Genome Resources": "#"
-                }
-            },
-            {
-                "main": "Community", 
-                "submain": {
-                    "Submit an Abstract": "#",
-                    "Dicty News": "#",
-                    "Listserv Archive": "#",
-                    "Colleagues profiles": "#",
-                    "Dicty Anual Conference": "#",
-                    "Job Opportunities": "#",
-                    "History": "#",
-                    "DictyLabs web": "#",
-                    "Pictures/Videos": "#",
-                    "dictyArt": "#",
-                    "Useful Links": "#",
-                    "Virtual Library": "#"
-                }
-            }
-        ];
+        $http.get('links.json').success(function(data) {
+            $scope.sections.menus = data;
+        });
     };
 
 })();
