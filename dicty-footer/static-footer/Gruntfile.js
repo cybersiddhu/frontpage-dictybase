224f6916e2ce1214a1892370713e5d89d3223d4f
David Jimenez-Morales <biodavidjm@gmail.com>
2014-10-20 10:50:32 -0500

    Updating gitignore and gruntfile
    Just testing that grunt is back and working properly noticed that
    .htaccess was not included in the gitignore file. Although I made sure
    that the task manager does not create it.

--- dicty-footer/static-footer/Gruntfile.js
+++ dicty-footer/static-footer/Gruntfile.js
@@ -288,10 +288,13 @@ module.exports = function (grunt) {
             '{,*/}*.html',
             'styles/fonts/{,*/}*.*'
           ]
-        }, {
-          src: 'node_modules/apache-server-configs/dist/.htaccess',
-          dest: '<%= config.dist %>/.htaccess'
-        }, {
+        }, 
+        // We don't need this for now
+        // {
+        //   src: 'node_modules/apache-server-configs/dist/.htaccess',
+        //   dest: '<%= config.dist %>/.htaccess'
+        // }, 
+        {
           expand: true,
           dot: true,
           cwd: 'bower_components/bootstrap/dist',
