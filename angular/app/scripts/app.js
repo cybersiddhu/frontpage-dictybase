fc4e2a988cf320ca24cd9a6ddfe02ddb80a3257e
David Jimenez-Morales <biodavidjm@gmail.com>
2014-11-20 13:07:52 -0600

    Adding the Header and Footer directive
    It is just to copy the `dictyHFdirective` folder into the `script/`
    folder, and just add the module `dictyHeaderFooterApp` to the `app.js`
    script

--- angular/app/scripts/app.js
+++ angular/app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'dictyHeaderFooterApp'
   ])
   .config(function ($routeProvider) {
     $routeProvider
